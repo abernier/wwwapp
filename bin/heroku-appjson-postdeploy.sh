6465abd806b61b00b6985886d3d99a21ba40d879
Antoine BERNIER <antoine.bernier@gmail.com>
2018-09-01 09:21:50 +0200

    Update heroku-appjson-postdeploy.sh

--- bin/heroku-appjson-postdeploy.sh
+++ bin/heroku-appjson-postdeploy.sh
@@ -3,3 +3,11 @@
 echo "Heroku Api tokk: $HEROKU_API_KEY"
 
 echo "Heroku app name: $HEROKU_APP_NAME"
+
+curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP_NAME/config-vars \
+  -d '{
+  "FOO": "bar",
+  "BAZ": "qux"
+}' \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3"
