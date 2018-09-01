a6fd2ac5a5add40c6d13c551e07cc431886ad01b
Antoine BERNIER <antoine.bernier@gmail.com>
2018-09-01 10:30:41 +0200

    Update heroku-appjson-postdeploy.sh

--- bin/heroku-appjson-postdeploy.sh
+++ bin/heroku-appjson-postdeploy.sh
@@ -4,13 +4,16 @@ echo "Heroku Api tokk: $HEROKU_API_KEY"
 
 echo "Heroku app name: $HEROKU_APP_NAME"
 
+#node -p "require('url').parse(process.env.REDIS_URL).host"
+
+
 curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP_NAME/config-vars \
-  -d '{
-  "FOO": "bar",
-  "BAZ": "qux"
-}' \
+  -d "{
+  \"wwwapp_postgres_uri\": \"$DATABASE_URL\",
+  \"wwwapp_redis_uri\": \"$REDIS_URL\"
+}" \
   -H "Content-Type: application/json" \
   -H "Accept: application/vnd.heroku+json; version=3" \
   -H "Authorization: Bearer $HEROKU_API_KEY"
 
-node -p "require('url').parse(process.env.REDIS_URL).host"
+
